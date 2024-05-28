ff393db652e0782dd9dade65b85fced108cb7eca
wny <viniberizzi@gmail.com>
2024-05-28 11:44:53 -0300

    setup web

---
+++ client/setup_flutter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Change directory to flutter if it exists, otherwise clone the repository
+if cd flutter; then
+  git pull
+  cd ..
+else
+  git clone https://github.com/flutter/flutter.git
+fi
+
+# List directory contents
+ls
+
+# Run flutter doctor
+./flutter/bin/flutter doctor
+
+# Clean flutter
+./flutter/bin/flutter clean
+
+# Switch to the beta channel
+./flutter/bin/flutter channel beta
+
+# Upgrade flutter
+./flutter/bin/flutter upgrade
+
+# Enable flutter web
+./flutter/bin/flutter config --enable-web
