0d09407a4ac999e421df347a545e4878d93e7573
wny <viniberizzi@gmail.com>
2025-07-18 07:47:55 -0300

    create new project

---
+++ README.md
@@ -0,0 +1,3 @@
+# dnd5e_dm_tools
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dnd5e_dm_tools/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dnd5e_dm_tools
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
